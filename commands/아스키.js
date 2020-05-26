73ad1ac071bb4e23d357451913b61f3d2ed7ce01
MOONKU852 <zone8601@gmail.com>
2020-05-26 17:19:16 +0900

    lolololololololo

--- commands/아스키.js
+++ commands/아스키.js
@@ -1,11 +1,11 @@
 const ascii = require('ascii-art');
 
 exports.run = (client, message, args, ops) => {
-    ascii.font(args.join(` `), "Doom", function(err, rendered) {
+    ascii.font(args.join(' '), 'Doom', function(rendered) {
         rendered = rendered.trimRight();
         if (rendered.length > 2000) return message.channel.send(`<@${message.author.id}> ` + '죄송해요, 글이 너무 길어요!');
         message.channel.send(rendered, {
-            code: "md"
+            code: 'md'
         });
 
     });
