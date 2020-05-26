caf3d59c271fc93165466c405f649bf2befccdc0
MOONKU852 <zone8601@gmail.com>
2020-05-26 17:36:24 +0900

    lolmath

--- commands/계산.js
+++ commands/계산.js
@@ -1,23 +1,24 @@
-const math = require('mathjs');
 const Discord = require('discord.js');
+const math = require('mathjs');
 
 exports.run = (client, message, args, tools) => {
 
-    if (!args[0]) return message.channel.send('Please input a calculation.');
+  if(!args[0]) return message.channel.send('Please input a calculation.')
 
-    let resp;
-    try {
-        resp = math.eval(args.join(' '));
-    } catch (e) {
-        return message.channel.send('Sorry, please input a valid calculation.');
-    }
+  let resp;
+  try {
+    resp = math.evaluate(args.join(' '))
+  } catch (e) {
+    return message.channel.send('Please enter a valid calculation.')
+  }
 
-    const embed = new Discord.RichEmbed()
-    .setColor(`#40e0d0`)
-    .setTitle('계산 결과')
-    .addField('Input', `\`\`\`js\n${args.join('')}\`\`\``)
-    .addField('Output', `\`\`\`js\n${resp}\`\`\``)
 
-    message.channel.send(embed);
+  const embed = new Discord.RichEmbed()
+    .setColor(0x808080)
+    .setTitle('Math Calculation')
+    .addField('Input', `\`\`\`css\n${args.join('')}\`\`\``)
+    .addField('Output', `\`\`\`css\n${resp}\`\`\``)
+
+message.channel.send(embed);
 
 }
